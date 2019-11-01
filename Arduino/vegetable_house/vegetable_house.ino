d27e7710ca808e173b293f8ff70f99434829bd0e
Vait-Marsovich <55848496+Vait-Marsovich@users.noreply.github.com>
2019-11-01 14:26:11 +0300

    Update vegetable_house.ino

--- Arduino/vegetable_house/vegetable_house.ino
+++ Arduino/vegetable_house/vegetable_house.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+// библиотека для работы с аналоговым термометром (Troyka-модуль)
+#include <TroykaThermometer.h>
 
+// создаём объект для работы с аналоговым термометром
+// и передаём ему номер пина выходного сигнала
+TroykaThermometer thermometer(A0);
+
+void setup()
+{
+  // открываем последовательный порт
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  // считываем данные с аналогового термометра
+  thermometer.read();
+  // вывод показателей аналогового термометра в градусах Цельсия
+  Serial.print("Temperature is ");
+  Serial.print(thermometer.getTemperatureC());
+  Serial.println(" C");
+  // вывод показателей аналогового термометра в градусах Кельвина
+  Serial.print("Temperature is ");
+  Serial.print(thermometer.getTemperatureK());
+  Serial.println(" K");
+  // вывод показателей аналогового термометра в градусах Фаренгейта
+  Serial.print("Temperature is ");
+  Serial.print(thermometer.getTemperatureF());
+  Serial.println(" F");
+  delay(1000);
 }
